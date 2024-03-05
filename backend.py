ac09623848d0b132c24f254ae8958b41acb223b0
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-03-06 02:56:37 +0530

    fix: summarize bug

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+from werkzeug.utils import secure_filename
 from summarize import summarize_pdf
 import os
 import logging
@@ -16,33 +17,27 @@ UPLOADS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads'
 
 @app.route('/')
 def index():
-    print("IM UNDDER INDEX FUCNTION")
     return render_template('index.html')
 
-def process_file(file):
-    print("FILE IS UPLOADED")
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(UPLOADS_DIR, filename)
+    file.save(file_path)
+    process_file(file_path)
+    return 'File uploaded successfully'
+
+def process_file(file_path):
     try:
-        # Create the 'uploads' directory if it doesn't exist
-        if not os.path.exists(UPLOADS_DIR):
-            app.logger.debug("Creating uploads directory...")
-            os.makedirs(UPLOADS_DIR)
-            app.logger.debug("Uploads directory created successfully.")
-
-        # Save the uploaded file to a temporary location
-        file_path = os.path.join(UPLOADS_DIR, file.filename)
-        file_path = file_path.replace('\\', '\\\\')  # Replace single backslashes with double backslashes
-        file.save(file_path)
-
         # Pass the file path to summarize_pdf function
         summary_text = summarize_pdf(file_path)
-
-        # Print the summary text for debugging
-        app.logger.debug("Summary text:", summary_text)
 
         # Emit the summary text to the frontend
         socketio.emit('summary_text', {'text': summary_text})
 
-        # Remove the temporary file
         os.remove(file_path)
 
     except Exception as e:
@@ -58,5 +53,4 @@ def handle_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    print("SERVER START")  # Print "SERVER START" when the script is executed
     socketio.run(app, debug=True)
